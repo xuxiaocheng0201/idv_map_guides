d1005cca0c2801b2968b09724622e7f0c30b31e4
XuXiaoCheng <2762267080@qq.com>
2026-09-01 02:41:29 +0800

    feat(page): 添加EntranceThumbnailPainter

---
+++ lib/painter/entrance_thumbnail_painter.dart
@@ -0,0 +1,116 @@
+import 'dart:math';
+
+import 'package:collection/collection.dart';
+import 'package:flutter/rendering.dart';
+import 'package:idv_map_guides/core/data.dart';
+import 'package:idv_map_guides/core/world.dart';
+import 'package:idv_map_guides/painter/world_painter.dart';
+
+class EntranceThumbnailPainter extends CustomPainter {
+  final World world;
+  final EntranceType entrance;
+  final int offsetMinX;
+  final int offsetMaxX;
+  final int offsetMinY;
+  final int offsetMaxY;
+
+  EntranceThumbnailPainter({
+    required this.world,
+    required this.entrance,
+    this.offsetMinX = -3,
+    this.offsetMaxX = 3,
+    this.offsetMinY = -3,
+    this.offsetMaxY = 3,
+  }): assert(offsetMinX < offsetMaxX),
+      assert(offsetMinY < offsetMaxY);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final width = offsetMaxX - offsetMinX + 1;
+    final height = offsetMaxY - offsetMinY + 1;
+    final cellSize = min(size.width / width, size.height / height);
+    drawBackground(canvas, width, height, cellSize);
+
+    final entrancePos = world.entrances[entrance]!;
+    final ex = entrancePos.x;
+    final ey = entrancePos.y;
+
+    for (int dx = offsetMinX; dx <= offsetMaxX; dx++) {
+      for (int dy = offsetMinY; dy <= offsetMaxY; dy++) {
+        final worldX = ex + dx;
+        final worldY = ey + dy;
+        final cell = world.cell(entrance.layer(), worldX, worldY);
+        if (cell == null || cell.id == null) continue;
+
+        final rect = Rect.fromLTWH(
+          (dx - offsetMinX) * cellSize,
+          (offsetMaxY - dy) * cellSize,
+          cellSize,
+          cellSize,
+        );
+
+        drawCell(canvas, rect, cell.isCorridor, cellSize);
+        if (cell.info.isStair != null) {
+          drawStair(canvas, rect, cell.info.isStair!, cellSize);
+        }
+        for (final direction in Direction.values) {
+          switch (cell.info.getEdgeType(direction)) {
+            case EdgeType.nothing:
+              final (dx2, dy2) = direction.dxy;
+              final neighbor = world.cell(entrance.layer(), worldX + dx2, worldY + dy2);
+              if (neighbor == null || neighbor.id != cell.id) {
+                drawWall(canvas, rect, direction, cellSize);
+              }
+              break;
+            case EdgeType.door:
+              drawDoor(canvas, rect, direction, cellSize);
+              break;
+            case EdgeType.innerWall:
+              drawWall(canvas, rect, direction, cellSize);
+              break;
+            case EdgeType.hole:
+              drawHole(canvas, rect, direction, cellSize);
+              break;
+          }
+        }
+
+        if (entrancePos == Position(x: worldX, y: worldY)) {
+          drawEntrance(canvas, rect, cellSize);
+        }
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant EntranceThumbnailPainter oldDelegate) {
+    return oldDelegate.world != world ||
+        oldDelegate.entrance != entrance ||
+        oldDelegate.offsetMinX != offsetMinX ||
+        oldDelegate.offsetMaxX != offsetMaxX ||
+        oldDelegate.offsetMinY != offsetMinY ||
+        oldDelegate.offsetMaxY != offsetMaxY;
+  }
+
+  BoolList getSignature() {
+    final list = BoolList.empty(growable: true);
+    final entrancePos = world.entrances[entrance]!;
+    final ex = entrancePos.x;
+    final ey = entrancePos.y;
+    for (int dx = offsetMinX; dx <= offsetMaxX; dx++) {
+      for (int dy = offsetMinY; dy <= offsetMaxY; dy++) {
+        final cell = world.cell(entrance.layer(), ex + dx, ey + dy);
+        if (cell == null || cell.id == null) {
+          list.add(false);
+        } else {
+          list.add(true);
+          list.add(cell.isCorridor);
+          list.add(cell.info.edgeNorth == EdgeType.door);
+          list.add(cell.info.edgeEast == EdgeType.door);
+          list.add(cell.info.edgeSouth == EdgeType.door);
+          list.add(cell.info.edgeWest == EdgeType.door);
+        }
+      }
+    }
+    return list;
+  }
+}
